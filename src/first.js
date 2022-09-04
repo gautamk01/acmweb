5cd4acd755fab78f35426acef2b3b4b94ec31f81
Gautam Krishna M <charmingdeveloper@protonmail.com>
2022-09-04 10:58:51 +0530

    Hi this is just a trai;

---
+++ src/first.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function First() {
+  return (
+    <div>
+      <div className="main-box">
+        <div className="mini-box"></div>
+        <button className="up">UP</button>
+        <button className="down">DOWN</button>
+      </div>
+    </div>
+  );
+}
+export default First;
